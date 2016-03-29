f68310f5718acc0441cffb0dadccfdf79a56257c
tu6619 <tu6619@my.bristol.ac.uk>
2016-03-29 18:03:37 +0100

    basic server set up

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Hapi = require('hapi')
+
+const server = new Hapi.Server()
+server.connection({
+  host: 'localhost',
+  port: 8000
+})
+
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, reply) => {
+    reply('hello')
+  }
+})
+
+server.start(() => console.log('server started'))
